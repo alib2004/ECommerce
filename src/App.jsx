6d424fcab219100815ae8c563c613d5eb8c4b977
alib2004 <aliw.b2004@gmail.com>
2025-07-27 16:03:54 +0330

    structure

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Shop from './pages/shop/Shop';
+import NotFound from './pages/404/NotFound';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+function App() {
+
+  return (
+    <>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about-us' element={<About/>}/>
+          <Route path='/contact-us' element={<Contact/>}/>
+          <Route path='/shop' element={<Shop/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-9xl'>هدر</h1>
+        </div>
+    );
+};
+
+export default Header;
