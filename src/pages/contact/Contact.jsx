f90df968e3dee89a72927fad63efc8a9496cd04f
alib2004 <aliw.b2004@gmail.com>
2025-08-07 15:52:55 +0330

    about us & contact us

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className='cont bg-khakestar-100 rounded-3xl'>
+            <div className='p-5 mt-10'>
+                <h1 className='font-bold !text-3xl'>تماس با ما</h1>
+                <span className='my-3 block'>قبل از مطرح کردن سوال، بخش سوالات متداول را مطاله نمایید.</span>
+                <div className='flex flex-col lg:flex-row gap-10 justify-between items-start'>
+                    <form className='flex flex-col gap-4 mt-3 w-full lg:w-[70%]'>
+                        <div className='flex flex-col lg:flex-row gap-2'>
+                            <input type="text" placeholder='نام شما' className='border px-3 py-2 border-tala rounded-2xl w-full'/>
+                            <input type="text" placeholder='شماره تماس شما' className='border px-3 py-2 border-tala rounded-2xl w-full'/>
+                        </div>
+                        <input type="email" placeholder='ایمیل شما' className='border px-3 py-2 border-tala rounded-2xl'/>
+                        <textarea placeholder='پیام شما'className='border px-3 py-2 border-tala rounded-2xl' rows={4}/>
+                        <button type='button' className='!bg-khakestar-300 w-fit !text-white px-3 py-2 self-end'>ارسال پیام</button>
+                    </form>
+                    <div className='lg:w-[30%]'>
+                        <div className='flex flex-col gap-4'>
+                            <div className='flex flex-col gap-2'>
+                                <span className='font-bold'>آدرس ایمیل:</span>
+                                <span className='text-tala'>aliw.b2004@gmail.com</span>
+                            </div>
+                            <div className='flex flex-col gap-2'>
+                                <span className='font-bold'>تلفن تماس:</span>
+                                <span className='text-tala'>09901232336</span>
+                            </div>
+                            <div className='flex flex-col gap-2'>
+                                <span className='font-bold'>آدرس:</span>
+                                <span className='text-tala'>تهران ، مهرآبادجنوبی</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 };
